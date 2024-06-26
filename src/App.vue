ee43af2a2bc478c9f18d6621755a4201f0076941
PAPAT16 <155905523+PAPAT16@users.noreply.github.com>
2024-06-26 12:04:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <!-- <p v-if="showResults">Reaction time: {{ score}} ms</p> -->
+  <result v-if="showResults" :score="score"/>
+</template>
+
+<script>
+import block from "./components/block.vue";
+import result from "./components/result.vue";
+
+export default {
+  name: "App",
+  components: { block, result},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score:null,
+      showResults:false
+    };
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      // console.log(this.delay);
+    },
+    endGame(reactionTime){
+      this.score =reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+  font-family: cursive
+}
+body{
+  background: #50ccad;
+}
+button{
+  /* background: #349b81; */
+  padding: 10px;
+  font-size: 20px;
+  border: none;
+  color: #50ccad;
+  border-radius: 20px;
+  text-align: center;
+  width: 150px;
+}
+button[disabled]{
+  background-color: #13493c;
+}
+</style>
---
+++ src/components/result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>reaction time {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Opoor fast finger 👍👍👍👌👌👍😂🤣";
+    } else if (this.score < 400) {
+      this.rank = "Opoor 😝😝😝😝🤣😂";
+    } else {
+      this.rank = "To lazy you can't carry your hand👎👎👎👎 😂😂😂";
+    }
+  }
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
